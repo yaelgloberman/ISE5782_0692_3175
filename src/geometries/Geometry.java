72c3e817dcfc142db19a2311a5d5c39b5f4eb1e9
Ruchama Simons <ruchama.simons@gmail.com>
2022-03-03 14:27:09 +0200

    exe1 finished!!!!!!! I hope

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -3,6 +3,14 @@ package geometries;
 import primitives.Point;
 import primitives.Vector;
 
+/**
+ * interface for all geometries
+ */
 public interface Geometry {
+    /**
+     *
+     * @param point
+     * @return the normal of the geometry
+     */
     Vector getNormal(Point point);
 }
